c27c40467a7a4337f370f6eadf73e255d64b1a6c
KYK <young.kwin@gmail.com>
2024-05-03 09:18:21 +0900

    init project

---
+++ src/test/java/com/GoShare/GoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GoShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
