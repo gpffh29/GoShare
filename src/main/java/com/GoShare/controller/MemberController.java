17acdfb75076e12eb5a4c3b2eba5b20beedb722e
gpffh29 <ma8679@skuniv.ac.kr>
2024-05-10 11:37:45 +0900

    [추가]회원가입 페이지 컨트롤러 및 css 원인: 로그인 페이지, 회원가입 페이지 컨트롤러 및 css작성 수정사항: 없음 영향 범위: 없음

--- src/main/java/com/GoShare/controller/MemberController.java
+++ src/main/java/com/GoShare/controller/MemberController.java
@@ -16,9 +16,24 @@ public class MemberController {
 
     private final MemberService memberService;
 
+//    회원가입 컨트롤러
     @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+
+
+//    로그인 컨트롤러
+    @GetMapping(value="/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+//    로그인 오류 메시지
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
     }
 }
