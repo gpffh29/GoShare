b6b5e18edfbcb473727ff6f99a24619c1fe46365
gpffh29 <ma8679@skuniv.ac.kr>
2024-05-31 16:43:27 +0900

    [구현]사진 업로드 구현5
    원인: 사진 업로드 구현5
    수정사항: 글 등록 버튼 url 설정
    영향 범위: 없음

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -93,18 +93,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // function bindDomEvent(){
-    //     $(".custom-file-input").on("change", function() {
-    //         var fileName = $(this).val().split("\\").pop();
-    //         var fileExt = fileName.substring(fileName.lastIndexOf(".")+1);
-    //         fileExt = fileExt.toLowerCase();
-    //
-    //         if(fileExt != "jpg" && fileExt != "jpeg" && fileExt != "gif" && fileExt != "png" && fileExt != "bmp"){
-    //             alert("이미지 파일만 등록이 가능합니다.");
-    //             return;
-    //         }
-    //         $(this).siblings(".custom-file-label").html(fileName);
-    //     });
-    // }
+    function bindDomEvent(){
+        $(".custom-file-input").on("change", function() {
+            var fileName = $(this).val().split("\\").pop();
+            var fileExt = fileName.substring(fileName.lastIndexOf(".")+1);
+            fileExt = fileExt.toLowerCase();
+
+            if(fileExt != "jpg" && fileExt != "jpeg" && fileExt != "gif" && fileExt != "png" && fileExt != "bmp"){
+                alert("이미지 파일만 등록이 가능합니다.");
+                return;
+            }
+            $(this).siblings(".custom-file-label").html(fileName);
+        });
+    }
 
 });
